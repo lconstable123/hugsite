84263f4e5e0c46d9609b0929e3aa6f656fe9b648
LUKECONSTABLE <luke@lukeconstable.com>
2025-12-14 20:52:01 +1100

    ere

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Plasma from "../components/Plasma";
 export default function Home() {
   return (
     <div className="">
+      {/* {replace react component here} */}
       <main className="flex min-h-screen bg-black w-full max-w-3xl flex-col items-center justify-between py-32 px-16  dark:bg-black sm:items-start">
         <div style={{ width: "100%", height: "600px", position: "relative" }}>
           <Plasma
